06accc7b9f23c1e1b70222d92db2e42a46592777
Bella_zhang <1406345211@qq.com>
2018-03-06 01:24:43 +0800

    添加项目文件

---
+++ src/page/login/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="login">
+    <header class="mui-bar mui-bar-nav">
+      <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+      <h1 class="mui-title">醒收银系统</h1>
+    </header>
+    <form class="form" @submit.prevent="submit">
+      <div class="line">
+        <input class="text" type="text" placeholder="用户名" >
+      </div>
+      <div class="line">
+        <input class="text" type="text" placeholder="密码" >
+      </div>
+      <div class="line">
+        <button class="btn">{{ status ? '正在登录中' : '登录' }}</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "login"
+    }
+</script>
+
+<style scoped>
+
+</style>
